5bc4ec3779f8566b99e959995794a3c3e075e6ec
ADRIAN-Komputer\ADRIAN <adriansmolakpiechocki@gmail.com>
2022-03-14 20:01:25 +0100

    zad3

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -5,18 +5,11 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-     class StandardGradeBook : BaseGradeBook
-    {
-
+     class StandardGradeBook : BaseGradeBook 
+    {  
         public StandardGradeBook(string name) : base(name)
-        {
-            
-            Type = GradeBookType.Standard;
-            
-        }
-            
-
-
-        
+        {      
+            Type = GradeBookType.Standard;  
+        }                
     }
 }
